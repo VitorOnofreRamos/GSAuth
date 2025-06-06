da4a4598b1edb9fdb1a8bdd240976e05e06a4bf8
Vitor Onofre Ramos <rm553241@fiap.com.br>
2025-06-05 23:00:27 -0300

    Update OrganizationController.cs

--- GSAuth/Controllers/ModelsController/OrganizationController.cs
+++ GSAuth/Controllers/ModelsController/OrganizationController.cs
@@ -1,7 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using GSAuth.DTOs;
+using GSAuth.Models;
+using GSAuth.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GSAuth.Controllers.ModelsController;
 
+[Route("api/[controller]")]
+[ApiController]
 public class OrganizationController : ControllerBase
 {
+    private readonly _IRepository<Organization> _repository;
+    private readonly IMapper _mapper;
+
+    public OrganizationController(_IRepository<Organization> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var organization = await _repository.GetAll();
+        return Ok(_mapper.Map<IEnumerable<OrganizationDTO>>(organization));
+    }
+
+    [HttpGet]
 }
